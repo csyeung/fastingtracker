e7adb3a9ea571e53ddeca1712dcb637d891fe169
Jonathan Yeung <jonathan.yeung@gmail.com>
2023-04-23 21:17:51 +0900

    setup first connection of koin in iOS

---
+++ shared/src/iosMain/kotlin/healthcare/app/fastingtracker/di/Helper.kt
@@ -0,0 +1,22 @@
+package healthcare.app.fastingtracker.di
+
+import healthcare.app.fastingtracker.Greeting
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+import org.koin.core.context.startKoin
+
+/**
+ * @author Jonathan YEUNG
+ * @since 2023-04-23
+ */
+fun initKoin() {
+    startKoin {
+        modules(appModules)
+    }
+}
+
+// Injection Bootstrap Helper
+class Helper : KoinComponent {
+    private val greeting: Greeting by inject()
+    fun greet(): String = greeting.greet()
+}
