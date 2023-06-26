8d29def025f834c9ec9bdd550ced073a6dbb20f3
Jonathan Yeung <jonathan.yeung@gmail.com>
2023-06-26 16:38:13 +0900

    adjust layout for Android

--- androidApp/src/main/java/healthcare/app/fastingtracker/android/MainActivity.kt
+++ androidApp/src/main/java/healthcare/app/fastingtracker/android/MainActivity.kt
@@ -3,23 +3,17 @@ package healthcare.app.fastingtracker.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Tab
 import androidx.compose.material.TabPosition
@@ -32,8 +26,6 @@ import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -41,12 +33,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.LottieConstants
@@ -57,13 +46,10 @@ import com.kizitonwose.calendar.core.OutDateStyle
 import com.kizitonwose.calendar.core.daysOfWeek
 import healthcare.app.fastingtracker.android.ui.Day
 import healthcare.app.fastingtracker.android.ui.DaysOfWeekTitle
-import healthcare.app.fastingtracker.android.ui.SegmentedControl
-import healthcare.app.fastingtracker.android.ui.state.MainUiState
-import kotlinx.coroutines.delay
+import healthcare.app.fastingtracker.android.ui.TimerScreen
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.component.KoinComponent
 import java.time.YearMonth
-import kotlin.time.Duration.Companion.seconds
 
 class MainActivity : ComponentActivity(), KoinComponent {
     companion object {
@@ -103,7 +89,7 @@ class MainActivity : ComponentActivity(), KoinComponent {
         ) {
             Column(modifier = Modifier.fillMaxHeight()) {
                 when (selectedTabIndex) {
-                    0 -> TimerScreen()
+                    0 -> TimerScreen(viewModel)
                     1 -> RecordScreen()
                     2 -> CalendarScreen()
                     3 -> SettingScreen()
@@ -212,194 +198,6 @@ class MainActivity : ComponentActivity(), KoinComponent {
                 .height(INDICATOR_HEIGHT)
                 .background(color = color)
         )
-    }
-
-    @Composable
-    fun TimerTaskComposable(state: MainUiState) {
-        if (state is MainUiState.Fasting) {
-            LaunchedEffect(Unit) {
-                while (true) {
-                    delay(1.seconds)
-                    viewModel.updateFasting()
-                }
-            }
-        }
-    }
-
-    @Composable
-    fun TimerScreen() {
-        val options = listOf(
-            getString(R.string.timeOption1),
-            getString(R.string.timeOption2),
-        )
-
-        val state = viewModel.uiState.collectAsState()
-        val record = viewModel.record.collectAsState()
-
-        TimerTaskComposable(state = state.value)
-
-        if (state.value == MainUiState.Loading) {
-            CircularProgressIndicator()
-        }
-
-        Column(modifier = Modifier
-            .fillMaxWidth()
-        ) {
-            SegmentedControl(
-                items = options,
-                useFixedWidth = true,
-                cornerRadius = 20,
-                itemWidth = 200.dp,
-                onItemSelection = {}
-            )
-        }
-
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-        ) {
-            Text(
-                modifier = Modifier
-                    .padding(start = 10.dp)
-                    .align(Alignment.Start),
-                text = getString(R.string.trackerTitleElapsedTime),
-                color = MaterialTheme.colors.primary,
-                fontSize = 30.sp,
-                fontFamily = FontFamily.Monospace,
-                fontWeight = FontWeight.SemiBold,
-            )
-
-            Text(
-                modifier = Modifier
-                    .padding(end = 10.dp)
-                    .align(Alignment.End),
-                text = record.value?.elapsedTime ?: getString(R.string.trackerPlaceholderTime1),
-                color = MaterialTheme.colors.primary,
-                fontSize = 28.sp,
-                fontFamily = FontFamily.Monospace,
-                fontWeight = FontWeight.Medium,
-            )
-        }
-
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-        ) {
-            Text(
-                modifier = Modifier
-                    .padding(start = 10.dp)
-                    .align(Alignment.Start),
-                text = getString(R.string.trackerTitleRemainingTime),
-                color = MaterialTheme.colors.primary,
-                fontSize = 30.sp,
-                fontFamily = FontFamily.Monospace,
-                fontWeight = FontWeight.SemiBold,
-            )
-
-            Text(
-                modifier = Modifier
-                    .padding(end = 10.dp)
-                    .align(Alignment.End),
-                text = record.value?.remainingTime ?: getString(R.string.trackerPlaceholderTime1),
-                color = MaterialTheme.colors.primary,
-                fontSize = 28.sp,
-                fontFamily = FontFamily.Monospace,
-                fontWeight = FontWeight.Medium,
-            )
-        }
-
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-        ) {
-            Text(
-                modifier = Modifier
-                    .padding(start = 10.dp)
-                    .align(Alignment.Start),
-                text = getString(R.string.trackerTitleStartTime),
-                color = MaterialTheme.colors.primary,
-                fontSize = 24.sp,
-                fontFamily = FontFamily.Monospace,
-                fontWeight = FontWeight.Normal,
-            )
-
-            Text(
-                modifier = Modifier
-                    .padding(end = 10.dp)
-                    .align(Alignment.End),
-                text = record.value?.startTime ?: getString(R.string.trackerPlaceholderTime2),
-                color = MaterialTheme.colors.primary,
-                fontSize = 20.sp,
-                fontFamily = FontFamily.Monospace,
-                fontWeight = FontWeight.Light,
-            )
-        }
-
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-        ) {
-            Text(
-                modifier = Modifier
-                    .padding(start = 10.dp)
-                    .align(Alignment.Start),
-                text = getString(R.string.trackerTitleEndTime),
-                color = MaterialTheme.colors.primary,
-                fontSize = 24.sp,
-                fontFamily = FontFamily.Monospace,
-                fontWeight = FontWeight.Normal,
-            )
-
-            Text(
-                modifier = Modifier
-                    .padding(end = 10.dp)
-                    .align(Alignment.End),
-                text = record.value?.endTime ?: getString(R.string.trackerPlaceholderTime2),
-                color = MaterialTheme.colors.primary,
-                fontSize = 20.sp,
-                fontFamily = FontFamily.Monospace,
-                fontWeight = FontWeight.Light,
-            )
-        }
-
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(20.dp)
-        ) {
-            OutlinedButton(
-                onClick = {
-                    when (state.value) {
-                        MainUiState.Fasting -> viewModel.stopFasting()
-                        else -> viewModel.startFasting()
-                    }
-                },
-                border = BorderStroke(
-                    1.dp,
-                    colorResource(id = R.color.colorButtonBackGroundDisable)
-                ),
-                shape = RoundedCornerShape(50),
-                colors = ButtonDefaults.outlinedButtonColors(contentColor = colorResource(id = R.color.colorButtonBackGroundDisable)),
-                contentPadding = PaddingValues(0.dp, 0.dp),
-                modifier = Modifier
-                    .padding(0.dp)
-                    .height(52.dp)
-                    .fillMaxWidth(0.5f)
-                    .align(Alignment.CenterHorizontally),
-            ) {
-                Text(
-                    text = getButtonText(state.value),
-                    color = colorResource(id = R.color.colorButtonBackGroundEnable),
-                    fontSize = 20.sp
-                )
-            }
-        }
-    }
-
-    private fun getButtonText(state: MainUiState) = when (state) {
-        is MainUiState.Start -> getString(R.string.trackerStart)
-        is MainUiState.Fasting -> getString(R.string.trackerEnd)
-        else -> getString(R.string.defaultEmptyMessage)
     }
 
     /**
---
+++ androidApp/src/main/java/healthcare/app/fastingtracker/android/ui/TimerScreen.kt
@@ -0,0 +1,201 @@
+package healthcare.app.fastingtracker.android.ui
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Card
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import healthcare.app.fastingtracker.android.MainViewModel
+import healthcare.app.fastingtracker.android.R
+import healthcare.app.fastingtracker.android.ui.state.MainUiState
+import kotlinx.coroutines.delay
+import kotlin.time.Duration.Companion.seconds
+
+@Composable
+fun TimerScreen(viewModel: MainViewModel) {
+    val options = listOf(
+        stringResource(R.string.timeOption1),
+        stringResource(R.string.timeOption2),
+    )
+
+    val state = viewModel.uiState.collectAsState()
+    val record = viewModel.record.collectAsState()
+
+    TimerTaskComposable(
+        state = state.value,
+        viewModel = viewModel
+    )
+
+    if (state.value == MainUiState.Loading) {
+        CircularProgressIndicator()
+    }
+
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .padding(
+            start = 5.dp,
+            end = 5.dp
+        )
+    ) {
+        // タイトル表示
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    top = 10.dp,
+                    bottom = 10.dp
+                ),
+            text = stringResource(id = R.string.app_name),
+            fontWeight = FontWeight.ExtraBold,
+            fontSize = 28.sp,
+            textAlign = TextAlign.Center
+        )
+
+        // TODO: 12:12 と 16:8 の選択肢を出せる
+//        SegmentedControl(
+//            items = options,
+//            useFixedWidth = true,
+//            cornerRadius = 20,
+//            itemWidth = 200.dp,
+//            onItemSelection = {}
+//        )
+
+        // 経過時間
+        InformationDisplay(
+            title = stringResource(R.string.trackerTitleElapsedTime),
+            text = record.value?.elapsedTime ?: stringResource(R.string.trackerPlaceholderTime1)
+        )
+
+        // 残り時間
+        InformationDisplay(
+            title = stringResource(R.string.trackerTitleRemainingTime),
+            text = record.value?.remainingTime ?: stringResource(R.string.trackerPlaceholderTime1)
+        )
+
+        // 開始時刻
+        InformationDisplay(
+            title = stringResource(R.string.trackerTitleStartTime),
+            text = record.value?.startTime ?: stringResource(R.string.trackerPlaceholderTime2)
+        )
+
+        // 終了時刻
+        InformationDisplay(
+            title = stringResource(R.string.trackerTitleEndTime),
+            text = record.value?.endTime ?: stringResource(R.string.trackerPlaceholderTime2)
+        )
+
+        Spacer(modifier = Modifier.padding(20.dp))
+
+        OutlinedButton(
+            onClick = {
+                when (state.value) {
+                    MainUiState.Fasting -> viewModel.stopFasting()
+                    else -> viewModel.startFasting()
+                }
+            },
+            border = BorderStroke(
+                1.dp,
+                colorResource(id = R.color.colorButtonBackGroundDisable)
+            ),
+            shape = RoundedCornerShape(50),
+            colors = ButtonDefaults.outlinedButtonColors(contentColor = colorResource(id = R.color.colorButtonBackGroundDisable)),
+            contentPadding = PaddingValues(0.dp, 0.dp),
+            modifier = Modifier
+                .padding(0.dp)
+                .height(52.dp)
+                .fillMaxWidth(0.5f)
+                .align(Alignment.CenterHorizontally),
+        ) {
+            Text(
+                text = getButtonText(state.value),
+                color = colorResource(id = R.color.colorButtonBackGroundEnable),
+                fontSize = 20.sp
+            )
+        }
+    }
+}
+
+@Composable
+fun TimerTaskComposable(state: MainUiState, viewModel: MainViewModel) {
+    if (state is MainUiState.Fasting) {
+        LaunchedEffect(Unit) {
+            while (true) {
+                delay(1.seconds)
+                viewModel.updateFasting()
+            }
+        }
+    }
+}
+
+@Composable
+fun InformationDisplay(title: String, text: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        elevation = 6.dp
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(
+                    top = 10.dp,
+                    bottom = 10.dp
+                )
+        ) {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 10.dp),
+                text = title,
+                color = Color.DarkGray,
+                fontSize = 24.sp,
+                fontFamily = FontFamily.Monospace,
+                fontWeight = FontWeight.SemiBold,
+                textAlign = TextAlign.Start
+            )
+
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(end = 10.dp),
+                text = text,
+                color = Color.Black,
+                fontSize = 20.sp,
+                fontFamily = FontFamily.Monospace,
+                fontWeight = FontWeight.Medium,
+                textAlign = TextAlign.End
+            )
+        }
+    }
+}
+
+@Composable
+fun getButtonText(state: MainUiState) = when (state) {
+    is MainUiState.Start -> stringResource(R.string.trackerStart)
+    is MainUiState.Fasting -> stringResource(R.string.trackerEnd)
+    else -> stringResource(R.string.defaultEmptyMessage)
+}
+
